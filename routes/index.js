a8786143dff56379f24876f7f1a6d039d1fe2498
devbywater <egaraudy@gmail.com>
2017-05-26 14:45:06 -0600

    Defined Create operation for Zone

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express Mutha Fucka' });
+})
+router.get('/createzone', function(req, res, next){
+  res.render('createzone', null)
+})
 
 module.exports = router;
